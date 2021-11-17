0bb02f0477a53a11e81828c5d07fc18605a12ac8
shomffl <showatanabe0226@gmail.com>
2021-11-17 21:30:50 +0900

    クラスごとにファイルを分割

---
+++ read.py
@@ -0,0 +1,23 @@
+from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
+
+
+
+class ReadCollection:
+
+    def __init__(self, collection_name, json_path):
+        self.collection_name = collection_name
+        self.json_path = json_path
+
+
+    def read(self):
+        cred = credentials.Certificate(self.json_path)
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+
+        docs = db.collection(self.collection_name).get()
+
+        for doc in docs:
+            data = doc.to_dict()
+            print(data)
---
+++ run.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+from crawling import CrawlingNews
+from add_firebase import AddFirebase
+from read import ReadCollection
+
+
+# ニュース記事のRSSのURL
+URL = "https://www.news24.jp/rss/index.rdf"
+
+# FirebaseのAPIキー
+load_dotenv()
+JSON_PATH = os.environ["FIREBASE_API_KEY"]
+
+news_data = CrawlingNews(URL)
+news_list = news_data.crawling()
+add_firebase = AddFirebase(news_list, JSON_PATH)
+collection_name = add_firebase.add_database()
+read_collection = ReadCollection(collection_name, JSON_PATH)
+read_collection.read()
