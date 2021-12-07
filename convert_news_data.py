c0fe73c863bdbb2d5724cb674dd14fdb56a5b9fe
shomffl <showatanabe0226@gmail.com>
2021-12-07 22:56:22 +0900

    コメントの追加

--- convert_news_data.py
+++ convert_news_data.py
@@ -1,5 +1,7 @@
 import json
 
+
+# クローリングしたニュースのデータをjson形式にしてフォルダーに格納する
 class ConvertNewsData:
 
     def __init__(self, news_data, file_path):
@@ -8,10 +10,17 @@ class ConvertNewsData:
 
     def convert(self):
         news_dict = {}
+
+        # 引数とした渡されたニュースデータの二次元配列の順番とデータを取得
         for num, data in enumerate(self.news_data):
+
+            # 対象のニュースが何番目かという情報を辞書のキーとして利用
             filename = f"news{num}"
+
+            # 空の辞書にニュースのタイトル、要約、リンクを追加していく
             news_dict[filename] = dict((["title", data[0]],["summary", data[1]],["link", data[2]]))
 
+        # 辞書型のデータをjson形式に変換して、指定されたパスに格納する
         with open(self.file_path , "w") as f:
             data = json.dumps(news_dict, sort_keys=True, indent=4)
             f.write(data)
