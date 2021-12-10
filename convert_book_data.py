588139b8a742344e12c60b84b49e9dd4a4e08e96
shomffl <showatanabe0226@gmail.com>
2021-12-10 20:25:06 +0900

    一つのjsonファイルに本のデータをまとめるように変更

--- convert_book_data.py
+++ convert_book_data.py
@@ -1,28 +1,39 @@
 import json
+from os import read
 from get_summary import GetSummary
 
 # 本の情報をjson形式に変換してフォルダーに格納するクラス
 class ConvertBookData:
 
-    def __init__(self, book_lists, folder_path):
+    def __init__(self, book_lists, filepath):
         self.book_lists = book_lists
-        self.folder_path = folder_path
+        self.filepath = filepath
 
     def convert(self):
+        # 本の情報を追加する際は、一度jsonファイル内の全てのデータを読み取って変数に格納してから、その変数に随時格納していく
+
+        # jsonファイル内が空の状態で読み取るとエラーが出るための例外処理を用意する
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as f:
+                read_data = json.load(f)
+                dict_book = read_data
+        except:
+            dict_book = {}
+
         # 複数の本のデータが「タイトル、ISBNコード」の順で二次元配列で渡される
         for data in self.book_lists:
-            dict_book = {}
             title = data[0]
             ISBN = data[1]
 
             # ISBNコードを使用してAPIを叩き本の要約を取得する
             summary = GetSummary(ISBN).get_suumary()
 
-            # 空の辞書にデータをそれぞれ格納する
-            dict_book["title"] = title
-            dict_book["summary"] = summary
+            # dict_bookにISBNコードをキーとしてデータを格納する
+            dict_book[ISBN] = dict((["title", title], ["summary", summary]))
 
-            # 指定されたフォルダーにISBNコードをファイル名としてデータを格納する
-            # データは辞書型からjson形式に変換する
-            with open(f"{self.folder_path}/{ISBN}.json", "w", encoding="utf-8") as f:
-                json.dump(dict_book, f, indent=4, ensure_ascii=False)
+
+
+        # 指定されたjsonファイルにデータを格納する
+        # データは辞書型からjson形式に変換する
+        with open(self.filepath, "w", encoding="utf-8") as f:
+            json.dump(dict_book, f, indent=4, ensure_ascii=False)
